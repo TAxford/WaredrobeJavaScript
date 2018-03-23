7d736d7541ea9473a555a2f1ae57b9fe5d6ecec7
TAxford <t.axford2@yahoo.com>
2018-03-23 12:19:04 -0400

    added prompt to Home page asking name, then inserting name into Welcome screen

--- WarDrobeJavaScript/WardrobeMVC/Content/JavaScript.js
+++ WarDrobeJavaScript/WardrobeMVC/Content/JavaScript.js
@@ -1,1 +1,8 @@
-﻿
+﻿/*Funtion to input users name into Title Welcoming them to the Waredrobe */
+function Greeting() {
+    var x = document.getElementById("jumboMain");    
+    var userResponse = prompt('Welcome to the Waredrobe. What is your name?');
+    alert(userResponse)
+    x.getElementsByTagName('h1')[0].innerText = "Welcome to the Wardrobe " + userResponse;
+    alert(userResponse)
+}
