38b725dd635d04428a1f2c473034ea3cb85b2af5
TAxford <t.axford2@yahoo.com>
2018-03-25 15:33:38 -0400

    Added hover over text changed to copy right in footer

--- WarDrobeJavaScript/WardrobeMVC/Content/JavaScript.js
+++ WarDrobeJavaScript/WardrobeMVC/Content/JavaScript.js
@@ -2,7 +2,17 @@
 function Greeting() {
     var x = document.getElementById("jumboMain");    
     var userResponse = prompt('Welcome to the Waredrobe. What is your name?');
-    alert(userResponse)
+    //alert(userResponse)
     x.getElementsByTagName('h1')[0].innerText = "Welcome to the Wardrobe " + userResponse;
-    alert(userResponse)
+    //alert(userResponse)
+}
+
+
+/*Function to change copy right in footer*/
+function overText(x){
+    x.innerText = "created by Timothy Joe Axford II"
+}
+
+function outText(x){
+    x.innerText = "&copy; @DateTime.Now.Year - AXFORD X CO";
 }
